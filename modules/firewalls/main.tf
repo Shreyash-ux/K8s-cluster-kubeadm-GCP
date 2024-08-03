c128adbe034d5cd023da44cd47acf866519bdd7a
Shreyash kerekar <shreyashkerekar@gmail.com>
2024-08-03 12:40:56 +0000

    alias

--- modules/firewalls/main.tf
+++ modules/firewalls/main.tf
@@ -3,7 +3,7 @@ resource "google_compute_firewall" "ssh" {
   network = var.network
   allow {
     protocol = "tcp"
-    ports    = ["22","179","6443"]
+    ports    = ["22"]
   }
   source_ranges = [ "0.0.0.0/0" ]
 
